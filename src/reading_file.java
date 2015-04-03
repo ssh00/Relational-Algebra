3b305ba48cfacd415a121a7d1f589ca021290257
yacinebenz <yacinebenz3@gmail.com>
2015-04-02 22:27:48 -0700

    add comments

--- src/reading_file.java
+++ src/reading_file.java
@@ -13,21 +13,21 @@ import java.util.Set;
 
 public class reading_file {
     public static void main(String [] args) throws IOException {
-        int i = 0, i2 = 0;
-        String name1 = null,name2 = null;
-        File is1 =new File("T1.txt");
-        File is2 =new File("T2.txt");
-        Scanner sc1 = new Scanner(is1);
-        Scanner sc2 = new Scanner(is2);
-        Map<Integer, String> map = new HashMap<Integer, String>();
+        int i = 0, i2 = 0; //initialize variables
+        String name1 = null,name2 = null; //initialize variables
+        File is1 =new File("T1.txt");    //reading file 1
+        File is2 =new File("T2.txt");   //reading file 2
+        Scanner sc1 = new Scanner(is1); //scanner for file 1
+        Scanner sc2 = new Scanner(is2); //scanner 2 for file 2
+        Map<Integer, String> map = new HashMap<Integer, String>(); //initializing hashmap
         Map<Integer, String> map2 = new HashMap<Integer, String>();
         Map<Integer, String> Union = new HashMap<Integer, String>();
-        Map<Integer, String> inter = new HashMap<Integer, String>(map);
-        Map<Integer, String> join = new HashMap<Integer, String>(map);
-        Map<Integer, String> diff = new HashMap<Integer, String>(map);
-        Map<Integer, String> diff2 = new HashMap<Integer, String>(map);
-        sc1.nextLine(); 
-        sc2.nextLine(); 
+        Map<Integer, String> inter = new HashMap<Integer, String>();
+        Map<Integer, String> join = new HashMap<Integer, String>();
+        Map<Integer, String> diff = new HashMap<Integer, String>();
+        Map<Integer, String> diff2 = new HashMap<Integer, String>();
+        sc1.nextLine(); //skip first line
+        sc2.nextLine(); //skip second line
 		        while (sc1.hasNext() && sc2.hasNext() && sc1.hasNextInt() && sc2.hasNextInt()) {
 		
 		            i = sc1.nextInt();
@@ -36,29 +36,32 @@ public class reading_file {
 		            name2 = sc2.next();
 		            map.put(i, name1);
 		            map2.put(i2, name2);
+		            /* 
+		            Comparing values
+		            */
+		            
 		            Union.putAll(map);
 		            Union.putAll(map2);
 		            for (Map.Entry<Integer, String> htEntries : map.entrySet()) {
-		                if(map2.containsKey(htEntries.getKey()) && map2.get(htEntries.getKey()).equals(htEntries.getValue())){
-		                    inter.put( htEntries.getKey(), htEntries.getValue());
+		                if(map2.containsKey(htEntries.getKey())) {
+		                  join.put( htEntries.getKey(), htEntries.getValue());
+		                if (map2.get(htEntries.getKey()).equals(htEntries.getValue())) {
+		                  inter.put(htEntries.getKey(), htEntries.getValue());
+		                } 
+		                } else {
+		                   diff.put( htEntries.getKey(), htEntries.getValue());
 		                }
 		            }
-		            for (Map.Entry<Integer, String> joinEntries : map.entrySet()) {
-			                if(map2.containsKey(joinEntries.getKey())){
-			                    join.put( joinEntries.getKey(), joinEntries.getValue());
-			                }
-		            }
-			        for (Map.Entry<Integer, String> diffEntries : map.entrySet()) {
-				                if(!map2.containsKey(diffEntries.getKey())){
-				                    diff.put( diffEntries.getKey(), diffEntries.getValue());
-				                } 
-			        }
-				    for (Map.Entry<Integer, String> diffEntries2 : map2.entrySet()) {
-					                if(!map.containsKey(diffEntries2.getKey())){
-					                    diff2.put( diffEntries2.getKey(), diffEntries2.getValue());
-					                }  
+
+		            for (Map.Entry<Integer, String> diffEntries2 : map2.entrySet()) {
+		                if(!map.containsKey(diffEntries2.getKey())){
+		                    diff2.put(diffEntries2.getKey(), diffEntries2.getValue());
+		                }
 		            }
 		        }
+		        /* 
+		        Print result 
+		        */
 		        System.out.println("T1 Union T2");
 	            System.out.println(Union);  
 	            System.out.println("T1 Intersection T2");
