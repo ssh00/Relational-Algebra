37727077d3d359b7a59b4ea6544306a9866d37fe
yacinebenz <yacinebenz3@gmail.com>
2015-04-04 00:33:58 -0700

    add comment

--- src/reading_file.java
+++ src/reading_file.java
@@ -28,37 +28,37 @@ public class reading_file {
         Map<Integer, String> diff2 = new HashMap<Integer, String>();
         sc1.nextLine(); //skip first line
         sc2.nextLine(); //skip second line
-		        while (sc1.hasNext() && sc2.hasNext() && sc1.hasNextInt() && sc2.hasNextInt()) {
+				        while (sc1.hasNext() && sc2.hasNext() && sc1.hasNextInt() && sc2.hasNextInt()) {
+				
+				            i = sc1.nextInt();
+				            i2 = sc2.nextInt();
+				            name1 = sc1.next();
+				            name2 = sc2.next();
+				            map.put(i, name1);
+				            map2.put(i2, name2);
+				            /* 
+				            Comparing values
+				            */
+				            
+				            Union.putAll(map);
+				            Union.putAll(map2);
+				            for (Map.Entry<Integer, String> htEntries : map.entrySet()) {
+				                if(map2.containsKey(htEntries.getKey())) {
+				                  join.put( htEntries.getKey(), htEntries.getValue());
+				                if (map2.get(htEntries.getKey()).equals(htEntries.getValue())) {
+				                  inter.put(htEntries.getKey(), htEntries.getValue());
+				                } 
+				                } else {
+				                   diff.put( htEntries.getKey(), htEntries.getValue());
+				                }
+				            }
 		
-		            i = sc1.nextInt();
-		            i2 = sc2.nextInt();
-		            name1 = sc1.next();
-		            name2 = sc2.next();
-		            map.put(i, name1);
-		            map2.put(i2, name2);
-		            /* 
-		            Comparing values
-		            */
-		            
-		            Union.putAll(map);
-		            Union.putAll(map2);
-		            for (Map.Entry<Integer, String> htEntries : map.entrySet()) {
-		                if(map2.containsKey(htEntries.getKey())) {
-		                  join.put( htEntries.getKey(), htEntries.getValue());
-		                if (map2.get(htEntries.getKey()).equals(htEntries.getValue())) {
-		                  inter.put(htEntries.getKey(), htEntries.getValue());
-		                } 
-		                } else {
-		                   diff.put( htEntries.getKey(), htEntries.getValue());
-		                }
-		            }
-
-		            for (Map.Entry<Integer, String> diffEntries2 : map2.entrySet()) {
-		                if(!map.containsKey(diffEntries2.getKey())){
-		                    diff2.put(diffEntries2.getKey(), diffEntries2.getValue());
-		                }
-		            }
-		        }
+				            for (Map.Entry<Integer, String> diffEntries2 : map2.entrySet()) {
+				                if(!map.containsKey(diffEntries2.getKey())){
+				                    diff2.put(diffEntries2.getKey(), diffEntries2.getValue());
+				                }
+				         }
+				        }
 		        /* 
 		        Print result 
 		        */
